66ff85d1bacb678b5698299dde38315e3f058fe2
Pedro Lima <pedrojppb@gmail.com>
2020-05-16 09:00:25 -0300

    operadores

---
+++ exerciciosJS/fundamentos/operadoresAritmeticos.js
@@ -0,0 +1,8 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d
+const subtração = b - d
+const multiplicacao = a* b
+const divisao = d / a
+const modulo = a % 2
+console.log(soma, subtração, multiplicacao, -divisao, modulo);
